f06300a56cacd24b1b1dcc8a0c713378f3a5315e
avinashChandavar7 <avinashChnadavar7@gmail.com>
2024-04-05 01:40:41 +0530

    fix : minor errors backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import cors from 'cors';
 import "dotenv/config";
 import cookieParser from "cookie-parser";
@@ -33,8 +33,15 @@ app.use("/api/v1/teacher", teacherRoutes)
 app.use("/api/v1/student", studentRoutes)
 
 
+
+
 const options = { explorer: true, }
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerOutput, options));
 
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(
+    path.join(__dirname, "../../frontend/dist/index.html")
+  );
+})
 
 export { app };
